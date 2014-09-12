0b116ab230baf64753a8d98b207e18180d4a0fd9
Marcin Grzejszczak <marcin@grzejszczak.pl>
2014-09-12 21:28:00 +0200

    Removed unnecessary caching

--- src/main/groovy/pl/warsjawa/microservice/Application.groovy
+++ src/main/groovy/pl/warsjawa/microservice/Application.groovy
@@ -1,10 +1,8 @@
 package pl.warsjawa.microservice
-
 import com.ofg.infrastructure.environment.EnvironmentSetupVerifier
 import groovy.transform.TypeChecked
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
-import org.springframework.cache.annotation.EnableCaching
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.EnableAspectJAutoProxy
@@ -17,7 +15,6 @@ import static com.ofg.config.BasicProfiles.*
 @EnableAutoConfiguration
 @EnableAspectJAutoProxy(proxyTargetClass = true)
 @ComponentScan(basePackages = ["pl.warsjawa.microservice", "pl.warsjawa.fraud"])
-@EnableCaching
 @EnableAsync
 class Application {
 
--- src/test/groovy/pl/warsjawa/fraud/AcceptanceSpec.groovy
+++ src/test/groovy/pl/warsjawa/fraud/AcceptanceSpec.groovy
@@ -1,9 +1,11 @@
 package pl.warsjawa.fraud
 
 import pl.warsjawa.base.MicroserviceMvcWiremockSpec
+import spock.lang.Unroll
 
 class AcceptanceSpec extends MicroserviceMvcWiremockSpec {
-           
+
+    @Unroll
     def "should mark job position as [#fraudResult] for a [#job] job"() {
         given: "a loan application with job position 'IT'"
         when: 'performing fraud detection'
