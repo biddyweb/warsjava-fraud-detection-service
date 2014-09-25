92cd6e220ed97e96521616ea9d858b076b11effb
Marcin Grzejszczak <marcin@grzejszczak.pl>
2014-09-26 00:55:18 +0200

    Added swagger ui 0.4.0 and the Missing Reactor aspect

--- src/main/groovy/pl/warsjawa/microservice/config/WebApplicationConfiguration.groovy
+++ src/main/groovy/pl/warsjawa/microservice/config/WebApplicationConfiguration.groovy
@@ -1,10 +1,11 @@
 package pl.warsjawa.microservice.config
 import com.ofg.infrastructure.config.WebAppConfiguration
+import com.ofg.infrastructure.reactor.ReactorInfraConfiguration
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Import
 
 @Configuration
-@Import(WebAppConfiguration)
+@Import([WebAppConfiguration, ReactorInfraConfiguration])
 class WebApplicationConfiguration {
 
 }
